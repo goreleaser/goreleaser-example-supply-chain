63d11ad7f8b6bcbd596dd3f9463d625f95dad565
Carlos A Becker <caarlos0@gmail.com>
2021-12-23 09:10:30 -0300

    chore: added a test
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"gopkg.in/yaml.v3"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	bts, err := yaml.Marshal(struct {
+		Name string
+		Age  int
+	}{
+		Name: "John",
+		Age:  30,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("\nyaml:\n\n%s\n", bts)
 }
---
+++ main_test.go
@@ -0,0 +1,11 @@
+package main
+
+func ExampleMain() {
+	main()
+	// Output:
+	// yaml:
+	//
+	// name: John
+	// age: 30
+	//
+}
